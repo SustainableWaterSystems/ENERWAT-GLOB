e586f07066157d14a3f232d3bceaa8a6848114a9
Magni <m.magni@uu.nl>
2025-10-28 16:00:45 +0100

    First commit ibwt

---
+++ source/water_abstraction/model/0_3_2_get_pumping_lift_height_infrastructure.R
@@ -0,0 +1,133 @@
+#pumping lift height is calculated as 
+#elevation of highest point on the transfer path 
+#minus
+#elevation of intake point
+
+library(dplyr)
+
+replaceMessage <- function(x, width = 80)
+{
+  message("\r", rep(" ", times = width - length(x)), "\r", appendLF = F)
+  message(x, appendLF = F)
+}
+
+inputDir <- '../../../../output/water_abstraction/model/ibwt/0_elevation_profiles/'
+
+inputDirElevation <- paste0(inputDir, '2_elevation_with_infrastructure/')
+
+outputDir <- paste0(inputDir, '3_pumping_elevations/')
+
+n.countries <- list.dirs(inputDirElevation, full.names = F, recursive = F)
+
+list.countries <- list()
+
+for(i in seq(length(n.countries))){
+  
+  country <- n.countries[i] 
+  
+  n.transfers <- list.dirs(paste0(inputDirElevation, country, '/'), full.names = F, recursive = F)
+  
+  list.transfers <- list()
+  
+  #### loop per transfer
+  for(j in seq(length(n.transfers))){
+    
+    transfer.name <- n.transfers[j]
+    
+    #### get elevation data of all transfer segments ####
+    segments.folder <- paste0(inputDirElevation, country, '/', 
+                              transfer.name, '/')
+    
+    segments.n <- list.files(segments.folder, full.names = F)
+    
+    list.segments <- list()
+    
+    for(k in seq(length(segments.n))){
+      
+      segment.elevation.data <- read.csv(paste0(segments.folder, segments.n[k]))
+      
+      intake.type <- segment.elevation.data$Subject[1]
+      
+      intake.elevation <- segment.elevation.data$elevation.aster.m[1]
+      
+      #### get various statistics of the segment
+      maximum.elevation <- max(segment.elevation.data$elevation.aster.m)
+      
+      minimum.elevation <- min(segment.elevation.data$elevation.aster.m)
+      
+      pumping.lift <- maximum.elevation - intake.elevation
+      
+      power.drop <- intake.elevation - minimum.elevation
+      
+      segment.length <- unique(segment.elevation.data$segment.length.km)
+      
+      maximum.location <- which(
+        segment.elevation.data$elevation.aster.m == maximum.elevation)
+      
+      minimum.location <- which(
+        segment.elevation.data$elevation.aster.m == minimum.elevation)
+      
+      if(length(maximum.location)>1){
+        maximum.location <- maximum.location[length(maximum.location)]
+      }
+      
+      distance.to.maximum <- segment.elevation.data$distance.km.total[maximum.location]
+      
+      distance.to.minimum <- segment.elevation.data$distance.km.total[minimum.location]
+      
+      pumping.df <- data.frame(
+        unique(segment.elevation.data$segment.full),
+        unique(segment.elevation.data$section.id),
+        unique(segment.elevation.data$segment.id),
+        intake.type,
+        intake.elevation,
+        maximum.elevation,
+        minimum.elevation,
+        pumping.lift,
+        power.drop,
+        segment.length,
+        segment.elevation.data$lon[1],
+        segment.elevation.data$lat[1],
+        segment.elevation.data$lon[maximum.location],
+        segment.elevation.data$lat[maximum.location],
+        distance.to.maximum,
+        distance.to.minimum
+        )
+     
+      colnames(pumping.df) <- c('segment.full', 'section.id', 'segment.id',
+                                'intake.type','intake.elevation',
+                                'maximum.elevation', 'minimum.elevation',
+                                'pumping.lift', 'power drop',
+                                'segment.length', 
+                                'intake.lon','intake.lat',
+                                'maximum.height.lon','maximum.height.lat', 
+                                'distance.to.maximum', 'distance.to.minimum')
+      
+      
+      list.segments[[k]] <- pumping.df 
+         
+    }
+    
+    segments.pumping.df <- do.call(rbind, list.segments)
+    
+    segments.pumping.df <- segments.pumping.df %>% 
+      mutate(transfer.name = transfer.name) %>% 
+      relocate(transfer.name, .before = segment.full)
+    
+    list.transfers[[j]] <- segments.pumping.df
+      
+  }
+  
+  country.transfers.df <- do.call(rbind, list.transfers)
+  
+  country.transfers.df <- country.transfers.df %>% 
+    mutate(Country = country) %>% 
+    relocate(Country, .before = transfer.name)
+  
+  list.countries[[i]] <- country.transfers.df
+  
+}
+
+pumping.dataframe.df <- do.call(rbind, list.countries)
+
+write.csv(pumping.dataframe.df, paste0(outputDir, 'intake_information_infrastructure.csv'), row.names = F)
---
+++ source/water_abstraction/model/2_1_analyse_pumping_output_yearly.R
@@ -0,0 +1,105 @@
+library(dplyr)
+library(vroom)
+
+inputDir <- '../../../../output/water_abstraction/model/ibwt/'
+
+inputDirElevation <- paste0(inputDir, '0_elevation_profiles/3_pumping_elevations/')
+inputDirEnergy <- paste0(inputDir, '2_energy/')
+
+
+#### load ####
+intake.data <- read.csv(paste0(inputDirElevation, 'intake_information_infrastructure.csv'))
+
+pumping.lift.data <- intake.data %>% 
+  filter(intake.type == 'Pumping station')
+
+n.countries <- list.dirs(inputDirEnergy, recursive = F, full.names = F)
+# n.countries
+
+
+countries.summary.list <- list()
+countries.ts.list <- list()
+
+for(i in seq(length(n.countries))){
+  
+  country <- n.countries[i]
+  
+  inputDirCountry <- paste0(inputDirEnergy, country, '/')
+  
+  #get country transfers
+  n.transfers <- list.dirs(inputDirCountry, recursive = F, full.names = F)
+  
+  transfers.summary.list <- list()
+  transfers.ts.list <- list()
+  
+  for(j in seq(length(n.transfers))){
+    
+    transfer <- n.transfers[j]
+    
+    inputDirTransfer <- paste0(inputDirCountry, transfer, '/')
+    
+    n.segments <- list.files(inputDirTransfer)
+    
+    segments.summary.list <- list()
+    segments.ts.list <- list()
+    
+    for(k in seq(length(n.segments))){
+      
+      segment <- n.segments[k]
+      
+      segment.energy <- read.csv(paste0(inputDirTransfer, segment))
+      
+      segment.energy.summary <- data.frame(
+        unique(segment.energy$segment.name),
+        mean(segment.energy$discharge.km3.y),
+        mean(segment.energy$energy.twh.low),
+        mean(segment.energy$energy.twh.high),
+        mean(segment.energy$energy.kwh.m3.low),
+        mean(segment.energy$energy.kwh.m3.high),
+        mean(segment.energy$energy.kwh.m3.km.low),
+        mean(segment.energy$energy.kwh.m3.km.high)
+      )
+      
+      colnames(segment.energy.summary) <- c(
+        'segment.name','mean.yearly.discharge',
+        'mean.twh.y.low','mean.twh.y.high',
+        'mean.kwh.m3.low','mean.kwh.m3.high',
+        'mean.kwh.m3.km.low','mean.kwh.m3.km.high'
+      )
+      
+      segments.ts.list[[k]] <- segment.energy
+      segments.summary.list[[k]] <- segment.energy.summary 
+      
+    }
+    
+    #### timeseries dataframe
+    segments.ts.df <- do.call(rbind, segments.ts.list)
+    
+    #### summary dataframe
+    segments.summary <- do.call(rbind, segments.summary.list)
+    
+    segments.summary.df <- segments.summary %>% 
+      mutate(transfer.name = transfer) %>% 
+      relocate(transfer.name, .before=segment.name)
+    
+    transfers.summary.list[[j]] <- segments.summary.df
+    transfers.ts.list[[j]] <- segments.ts.df
+    
+  }
+  
+  transfers.summary <- do.call(rbind, transfers.summary.list)
+  
+  transfers.summary.df <- transfers.summary %>% 
+    mutate(Country = country) %>% 
+    relocate(Country, .before=transfer.name)
+  
+  countries.summary.list[[i]] <- transfers.summary.df
+  countries.ts.list[[i]] <- do.call(rbind, transfers.ts.list)
+  
+}
+
+pumping.summary <- do.call(rbind, countries.summary.list)
+pumping.ts.all <- do.call(rbind, countries.ts.list)
+
+write.csv(pumping.summary, paste0(inputDirEnergy, 'pumping_summary.csv'), row.names = F)
+vroom_write(pumping.ts.all, paste0(inputDirEnergy, 'pumping_timeseries_all.csv'), delim = ',')
