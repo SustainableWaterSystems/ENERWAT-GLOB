a87d238fd9b738fa5a8a2041b798ddd735c9a193
Magni <m.magni@uu.nl>
2026-03-16 11:42:39 +0100

    hypnotic funky fresh

---
+++ source/water_abstraction/visualization/ibwt/0_plot_ibwt_locations.R
@@ -0,0 +1,106 @@
+rm(list=ls())
+
+library(dplyr)
+library(ggplot2)
+library(ggthemes)
+library(patchwork)
+
+palette.purposes <- c("#7FC97F", "#AFF80A","#BEAED4", "#FDC086",
+                       "#386CB0", "#F0027F", '#332288', '#117733', 
+                       '#88CCEE', '#DDCC77', '#CC6677', '#AA4499', '#882255')
+
+palette.purposes <- c('#88CCEE', "#FDC086", '#44AA99','#CC6677')
+
+#### load ####
+inputDir <-
+  '../../../../output/water_abstraction/model/ibwt/1_discharge_timeseries/0_timeseries_raw/'
+
+outputDir <- '../../../../output/water_abstraction/visualization/ibwt/'
+
+#### ####
+ibwt.info <- read.csv(paste0(inputDir, '1_0_bias_correction_sections.csv'))
+ibwt.segments <- read.csv(paste0(inputDir, '1_1_bias_correction_segments.csv'))
+
+wg <- map_data("world")
+
+ibwt.names <- unique(ibwt.info$transfer.name)
+
+####
+source('fun_0_make_ibwt_locations_plot_data.R')
+
+#### plot ####
+###Inter-basin transfer locations
+p.map <- ggplot() +
+  geom_map(
+    data = wg, map = wg,
+    aes(long, lat, map_id = region),
+    color = "white", fill= "grey", alpha=0.6
+  ) +
+  geom_point(data = ibwt.plot.df, 
+             aes(
+               x = ibwt.from.lon, y = ibwt.from.lat, shape = purpose.main,
+               fill = transfer.length, size = n.reservoirs),
+             alpha = 0.7, stroke = 1)+
+  ylim(-55,70) +
+  theme_map() +
+  theme(legend.position = 'bottom',
+        legend.direction = 'vertical',
+        legend.justification = 'center',
+        legend.spacing= unit(1.0, 'cm'),
+        legend.title = element_text(hjust=0.5, size=14),
+        legend.title.position = 'top',
+        legend.text = element_text(size=13)) +
+  scale_shape_manual(values =c(21,22,23,24),
+                     name = 'Main purpose') +
+  scale_fill_gradient(low = 'darkolivegreen4', high = 'gold1',
+                      limits = c(0,1500)) +
+  scale_size(name="Number of reservoirs",
+             breaks=c(1,3,5,7,10),
+             range=c(2,6)) +
+  guides(color=guide_legend(title="Main purpose"),
+         fill=guide_colorbar(title="Project length (km)", direction = 'horizontal',
+                            label.position = 'bottom',
+                            theme = theme(
+                              legend.key.width  = unit(8, "lines"))),
+         size=guide_legend(title='Number of reservoirs', direction = 'horizontal',
+                           label.position = 'bottom')
+         )
+
+#### cumulative length over time
+
+p.length <- ggplot() + 
+  geom_line(ibwt.cumulative.stats, mapping = aes(x=initial.year, y= cumulative.length)) +
+  geom_col(ibwt.cumulative.stats, mapping = aes(x=initial.year, y = length.year)) +
+  ggtitle('A.') +
+  xlab('Year') +
+  ylab('km') +
+  theme_light() 
+
+p.reservoirs <- ggplot() + 
+  geom_line(ibwt.cumulative.stats, mapping = aes(x=initial.year, y= cumulative.reservoirs)) +
+  geom_col(ibwt.cumulative.stats, mapping = aes(x=initial.year, y = reservoirs.year)) +
+  ggtitle('B.') +
+  xlab('Year') +
+  ylab('(-)') +
+  theme_light() 
+
+
+p.purposes <- ggplot() + 
+  geom_point(pdf, mapping = aes(x=initial.year, y= cs, shape = group)) +
+  geom_line(pdf, mapping = aes(x=initial.year, y= cs, shape= group)) +
+  scale_color_manual(values=palette.purposes) +
+  scale_shape_manual(values =c(21,22,23,24)) +
+  ggtitle('C.') +
+  xlab('Year') +
+  ylab('(-)') +
+  theme_light() +
+  theme(legend.position = 'none')
+
+p.patch <- p.map / (p.length + p.reservoirs + p.purposes) + 
+  plot_layout(heights = c(3, 1))
+
+
+ggsave(paste0(outputDir,'ibwt_locations_purposes.png'), p.patch,
+       height=7, width=7, units='in', dpi=300, bg='white')
+
+file.show(paste0(outputDir,'ibwt_locations_purposes.png'))
---
+++ source/water_abstraction/visualization/ibwt/1_plot_discharge_types_map.R
@@ -0,0 +1,60 @@
+rm(list=ls())
+
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(lubridate)
+library(ggtext)
+library(ggthemes)
+library(patchwork)
+
+
+palette.timeseries <- c("#000000", '#648FFF', '#FFB000','#FE6100') 
+palette.map <- c('#88CCEE','#44AA99', '#785EF0', '#AA4499')
+
+inputDir <- '../../../../output/water_abstraction/model/ibwt/'
+inputDirElevation <- paste0(inputDir, '0_elevation_profiles/3_information/')
+inputDirDischarge <- paste0(inputDir, '1_discharge_timeseries/')
+inputDirEnergy <- paste0(inputDir, '2_energy/')
+outputDirViz <- '../../../../output/water_abstraction/visualization/ibwt/'
+
+wg <- map_data("world")
+
+
+#### load ####
+sections.coord <- read.csv(paste0(inputDirDischarge, '0_timeseries_raw/0_information_sections.csv'))
+
+section.info.df <- read.csv(
+  paste0(inputDirDischarge, '0_timeseries_raw/1_0_bias_correction_sections.csv'))
+sections.reservoirs <- read.csv(
+  paste0(inputDirElevation, '0_0_sections_reservoirs.csv'))
+
+discharge.monthly.df <- read.csv(
+  paste0(inputDirDischarge, 
+         '1_bias_corrected/0_0_discharge_bias_corrected_monthly.csv')) %>% 
+  mutate(datetime = as.Date(datetime)) %>% 
+  mutate(
+    year.continuous = (year(datetime)),
+    year.factor = factor(year(datetime)),     # use year to define separate curves
+    date = update(datetime, year = 1))  # use a constant year for the x-axis
+
+discharge.yearly.df <- read.csv(
+  paste0(inputDirDischarge, 
+         '1_bias_corrected/0_1_discharge_bias_corrected_yearly.csv')) %>% 
+  mutate(datetime = as.Date(datetime))
+
+discharge.types <- read.csv(
+  paste0(inputDirEnergy, 
+         '5_0_discharge_by_intake_type_timeseries.csv')) %>% 
+  mutate(datetime = as.Date(datetime))
+
+#### ####
+
+source('fun_1_0_plot_discharge_timeseries_types.R')
+source('fun_1_1_plot_discharge_map.R')
+
+discharge.ts.map <- p.discharge.y.types / p.map.discharge
+
+ggsave(paste0(outputDirViz,'ibwt_discharge_ts_map.png'), discharge.ts.map,
+       height=11, width=8, units='in', dpi=300, bg='white')
+file.show(paste0(outputDirViz,'ibwt_discharge_ts_map.png'))
